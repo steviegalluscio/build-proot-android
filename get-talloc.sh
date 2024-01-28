a862569aef25281d610533f5d9434aea74e25e68
steviegalluscio <galluscio.stevie@gmail.com>
2024-01-27 23:18:34 -0500

    Update get-talloc.sh

--- get-talloc.sh
+++ get-talloc.sh
@@ -10,3 +10,5 @@ cd "$BUILD_DIR"
 if [ -d "talloc-$TALLOC_V" ] ; then exit 0 ; fi
 
 wget -O - "https://download.samba.org/pub/talloc/talloc-$TALLOC_V.tar.gz" | tar -xzv
+
+cp ../os2_delete_patched.c "talloc-$TALLOC_V/lib/replace/tests/os2_delete.c"
